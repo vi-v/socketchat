72009052cba0c22f749683fb966ecdcb4e9f15bc
Vishnu Vijayan <vvijayan@purdue.edu>
2017-06-05 17:53:00 +0530

    Create server

---
+++ server/socketchat.js
@@ -0,0 +1,17 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+io.on('connection', socket => {
+    console.log('User connected');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+
+    socket.on('add-message', message => {
+        io.emit('message', { type: 'new-message', text: message });
+    });
+});
+
+http.listen(8000, () => console.log('Server running on port 8000'));
